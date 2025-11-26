b29b3d97890a4818aa9cf37d363ee0465f9855fc
Yashraj Sharma <yashrajsharma12012002@gmail.com>
2025-11-26 16:01:02 +0530

    Streaming-update

--- Chat_Bot/Langgraph_Backend_Chatbot.py
+++ Chat_Bot/Langgraph_Backend_Chatbot.py
@@ -2,10 +2,10 @@ from langgraph.graph import StateGraph, START , END
 from typing import TypedDict, Annotated , List
 from pydantic import BaseModel , Field
 from langchain_groq import ChatGroq
-from langchain_core.messages import HumanMessage , BaseMessage ### all the Messages like system message, Ai meassages are inherited from BAse message... 
+from langchain_core.messages import HumanMessage , BaseMessage
 import os
 import operator
-from langgraph.graph.message import add_messages ### Reducer... similar to oprerator
+from langgraph.graph.message import add_messages
 from langgraph.checkpoint.memory import MemorySaver
 import asyncio
 
@@ -17,7 +17,7 @@ os.environ["PYTORCH_JIT"] = "0"
 # Compatible with Streamlit & python 3.10+
 try:
     loop = asyncio.get_event_loop()
-except RuntimeError:  # No loop in this thread
+except RuntimeError:
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
 
@@ -25,33 +25,27 @@ if loop.is_closed():
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
 
-### Model Definition:
-model=ChatGroq(
+model = ChatGroq(
     model="llama-3.1-8b-instant",
     api_key=os.getenv("GROQ_API_KEY")
 )
 
-### State Defintion:
 class ChatState(TypedDict):
-    messages:Annotated[List[BaseMessage],add_messages]
+    messages: Annotated[List[BaseMessage], add_messages]
+
+def llm_chat(state: ChatState):
+    message = state['messages']
+    response = model.invoke(message)
+    return {'messages': response}
+
+check_pointer = MemorySaver()
+graph = StateGraph(state_schema=ChatState)
+
+graph.add_node("llm_chat", llm_chat)
+graph.add_edge(START, "llm_chat")
+graph.add_edge("llm_chat", END)
 
 
-def llm_chat(state:ChatState):
-    message=state['messages']
-    response=model.invoke(message)
-    return {'messages':response}
+graph.set_entry_point("llm_chat")  
 
-
-### Defining Graph and CheckPointer:
-check_pointer=MemorySaver()
-graph=StateGraph(state_schema=ChatState)
-
-### Adding Nodes:
-graph.add_node("llm_chat",llm_chat)
-
-### Adding Edges:
-graph.add_edge(START,"llm_chat")
-graph.add_edge('llm_chat',END)
-
-### Compiling Graph:
-workflow=graph.compile(checkpointer=check_pointer)
+workflow = graph.compile(checkpointer=check_pointer)
--- Chat_Bot/Streamlit_Frontend_Chatbot.py
+++ Chat_Bot/Streamlit_Frontend_Chatbot.py
@@ -3,29 +3,35 @@ from Langgraph_Backend_Chatbot import workflow
 from langchain_core.messages import HumanMessage , BaseMessage
 from langchain_groq import ChatGroq
 
-thread_id="1"
-configuration={'configurable':{'thread_id':thread_id}}
+config={'configurable':{'thread_id':'thread-1'}}
 
-if 'message_history' not in st.session_state: ## St.session_state is basically a dictionary how data do not get delete at the time we press enter again...
-    st.session_state['message_history']=[]
+if 'message_history' not in st.session_state:
+    st.session_state['message_history'] = []
 
-#Loading the Conversation History
+# Loading the Conversation History
 for message in st.session_state['message_history']:
     with st.chat_message(message['role']):
         st.text(message['content'])
 
-user_input=st.chat_input('user')
+user_input = st.chat_input('user')
 
 if user_input:
 
-    #adding to the History first:
     st.session_state['message_history'].append({'role':'user','content':user_input})
+
     with st.chat_message('user'):
         st.text(user_input)
-    
 
-    response=workflow.invoke({"messages":[HumanMessage(content=user_input)]},config=configuration)
-    output=response['messages'][-1].content
-    st.session_state['message_history'].append({'role':'assistant','content':output})
+    def stream_response():
+        for message_chunk, metadata in workflow.stream(
+            {"messages":[HumanMessage(content=user_input)]},
+            config={'configurable':{'thread_id':'thread-1'}},
+            stream_mode='messages'
+        ):
+            # message_chunk is a BaseMessage (AIMessage)
+            yield message_chunk.content
+
     with st.chat_message('assistant'):
-        st.text(output)
+        ai_message = st.write_stream(stream_response())
+
+    st.session_state['message_history'].append({'role':'assistant','content':ai_message})
